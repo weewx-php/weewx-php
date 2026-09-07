0bb26b9774930c6a21ee8cd04376ddc35e498702
Manuel <hilman2@gmail.com>
2026-09-07 21:03:34 +0200

    Release core 0.2.0

--- src/Version.php
+++ src/Version.php
@@ -7,7 +7,7 @@ namespace WeewxPhp;
 /** What this application calls itself to the services it posts to. */
 final class Version
 {
-    public const STRING = '0.2.0-beta.1';
+    public const STRING = '0.2.0';
 
     /** What goes into an HTTP `User-Agent` and a CWOP login line. */
     public const NAME = 'weewx-php';
